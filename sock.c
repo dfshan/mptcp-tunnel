f6ebc4a1083332465713101f0681d6c101b644be
Danfeng Shan <shandanf@gmail.com>
2017-01-02 10:59:21 +0800

    small bug

--- sock.c
+++ sock.c
@@ -6,7 +6,9 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-/******************************** 
+#include "sock.h"
+
+/********************************
  * Client/server helper functions
  ********************************/
 /*
@@ -14,7 +16,7 @@
  *	 return a socket descriptor ready for reading and writing. This
  *	 function is reentrant and protocol-independent.
  *
- *	 On error, returns: 
+ *	 On error, returns:
  *	   -2 for getaddrinfo error
  *	   -1 with errno set for other errors.
  */
@@ -32,21 +34,21 @@ int open_clientfd(char *hostname, char *port) {
 		fprintf(stderr, "getaddrinfo failed (%s:%s): %s\n", hostname, port, gai_strerror(rc));
 		return -2;
 	}
-  
+
 	/* Walk the list for one that we can successfully connect to */
 	for (p = listp; p; p = p->ai_next) {
 		/* Create a socket descriptor */
-		if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) 
+		if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
 			continue; /* Socket failed, try the next */
 
 		/* Connect to the server */
-		if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1) 
+		if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1)
 			break; /* Success */
 		if (close(clientfd) < 0) { /* Connect failed, try another */  //line:netp:openclientfd:closefd
 			fprintf(stderr, "open_clientfd: close failed: %s\n", strerror(errno));
 			return -1;
-		} 
-	} 
+		}
+	}
 
 	/* Clean up */
 	freeaddrinfo(listp);
@@ -55,3 +57,36 @@ int open_clientfd(char *hostname, char *port) {
 	else	/* The last connect succeeded */
 		return clientfd;
 }
+
+int open_listenfd(int port)
+{
+	int listenfd, optval=1;
+	struct sockaddr_in saddr;
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd < 0) {
+		fprintf(stderr, "create socket failed: %s\n", strerror(errno));
+		return listenfd;
+	}
+	/* Eliminates "Address already in use" error from bind */
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+		(const void *)&optval , sizeof(int));
+
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	saddr.sin_port = htons(port);
+
+	if (bind(listenfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+		fprintf(stderr, "bind() failed: %s\n", strerror(errno));
+		if (close(listenfd) < 0) {
+			fprintf(stderr, "open_listenfd close failed: %s\n", strerror(errno));
+		}
+		return -1;
+	}
+	if (listen(listenfd, LISTENQ) < 0) {
+		close(listenfd);
+		return -1;
+	}
+	return listenfd;
+}
+/* $end open_listenfd */
--- sock.h
+++ sock.h
@@ -1,6 +1,9 @@
 #ifndef SOCK_H
 #define SOCK_H
 
+#define LISTENQ 5
+
 int open_clientfd(char *hostname, char *port);
+int open_listenfd(int port);
 
 #endif
